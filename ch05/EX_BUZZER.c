04135e6f585472cd0137f475c1efaa835f3f1d83
Cha-Ji <chajiwon1007@pukyong.ac.kr>
2022-04-08 12:02:48 +0900

    Add pwm buzzer Elise

--- ch05/EX_BUZZER.c
+++ ch05/EX_BUZZER.c
@@ -4,13 +4,18 @@
 #define LED_PWM_PIN		1
 #define BUZ_PIN			0
 
-unsigned int melody[8] = {262, 294, 330, 349, 392, 440, 494, 523};
+unsigned int melody[14] = {262, 294, 330, 349, 392, 440, 494,
+						523, 587, 659, 698, 784, 830, 987};
+int score[9] = {9, 8, 9, 8, 9, 6, 8, 7, 5};
 
 void setPinMode();
 void runLoop();
+void setScore();
 
 int main(void) {
 	wiringPiSetup();
+	
+	setPinMode();
 
 	softToneCreate(BUZ_PIN);
 
@@ -21,11 +26,16 @@ int main(void) {
 	return 0;
 }
 
+void setPinMode() {
+	pinMode(BUZ_PIN, SOFT_TONE_OUTPUT);
+}
+
 void runLoop() {
 	while(1) {
-		for(int i = 0; i < 8; i++) {
-			softToneWrite(BUZ_PIN, melody[i]);
-			delay(1000);
+		for(int i = 0; i < 9; i++) {
+			softToneWrite(BUZ_PIN, melody[score[i]]);
+			delay(500);
 		}
+		delay(500);
 	}
 }
