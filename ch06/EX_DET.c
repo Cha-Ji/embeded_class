077d3e5c4ef0b4163b851325455727a26ccb8e13
Cha-Ji <chajiwon1007@pukyong.ac.kr>
2022-04-15 12:09:22 +0900

    Week06: Add detect LCD

---
+++ ch06/EX_DET.c
@@ -0,0 +1,40 @@
+#include <wiringPi.h>
+#include <lcd.h>
+#include <stdio.h>
+
+#define LCD_RS 11 // register select pin
+#define LCD_E  10 // Enable pin
+#define LCD_D4 6  // Data pin 4
+#define LCD_D5 5  // Data pin 5
+#define LCD_D6 4  // Data pin 6
+#define LCD_D7 1  // Data pin 7
+
+#define PIR_PIN 29
+
+#define DELAY_TIME 1000
+int main(void) {
+	int lcd;
+	wiringPiSetup();
+
+	pinMode(PIR_PIN, INPUT);
+
+	if (lcd = lcdInit(2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0)) {
+		printf("lcd init failed! \n");
+		return -1;
+	}
+
+	lcdPosition(lcd, 0, 0); //Position cursor on the first line in the first column.
+	lcdPuts(lcd, "HELLO WORLD");
+
+	while(1) {
+		if(digitalRead(PIR_PIN)) lcdPuts(lcd, "Detected");
+		else lcdPuts(lcd, "------");
+
+		delay(DELAY_TIME);
+		lcdClear(lcd);
+	}
+
+
+	return 0;
+}
+
